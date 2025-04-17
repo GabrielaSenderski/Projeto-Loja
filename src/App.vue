f64a3b522300ae2e48cc34a5519cb0d45075fab8
GabrielaSenderski <gabrielasenderski.ifc@gmail.com>
2025-04-17 09:32:30 -0300

    FEAT: Nav bar

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,28 @@
 </script>
 
 <template>
+<script setup>
+
+</script>
+
+<template>
+<body>
+    <header>
+        <nav>
+        <img src="../src/components/Logo.png" alt="Logo IFBooks">
+        <input type="text" placeholder="Pesquisar">
+        <ul>
+            <li><a href="#">Termos</a></li>
+            <li><a href="#">Equipe</a></li>
+        </ul>
+        </nav>
+    </header>
+</body>
+</template>
+
+<style scoped>
+
+</style>
 
 </template>
 
