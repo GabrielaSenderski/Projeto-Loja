6127e422a2d99cc833db6ab4ebdccac2d87292a2
Hen0042007 <henrique.santiago.sb@gmail.com>
2025-04-22 09:29:44 -0300

    FEAT: Criação da pagina do carrinho

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,64 @@
-<script setup>
+  <script setup>
 
 </script>
 
 <template>
 
+<section>
+
+</section>
+<main>
+
+<section class="carrinho">
+  <h2>Carrinho</h2>
+  <div>
+    <ul>
+      <li>Titulo</li>
+      <li>Quantidade</li>
+      <li>Subtotal</li>
+    </ul>
+    <hr>
+
+
+    <hr>
+
+
+  </div>
+</section>
+<section>
+  <button>Voltar para a loja</button>
+  <div>
+    <input type="text" placeholder="Codigo de cupom">
+    <button>Inserir cupom</button>
+  </div>
+  <div>
+    <h4>Total da Compra</h4>
+    <ul>
+      <li>Produtos</li>
+      <hr>
+      <li>Frete</li>
+      <hr>
+      <li>Total</li>
+    </ul>
+    <button>Ir para o pagamento</button>
+  </div>
+</section>
+</main>
+<footer>
+  <div>
+  IFBooks
+</div>
+<div>
+  Contato
+  <ul>
+    <li>+55 47 40045263</li>
+    <li>8h às 23h - Seg a Sex</li>
+    <li>Contato@ifbooks.com</li>
+  </ul>
+</div>
+<hr>
+<p>&copy; Alguns direitos reservados. IFbooks 2025.</p>
+</footer>
 </template>
 
 <style scoped>
